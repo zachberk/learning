6a50d56a1cf8282ff8da58ebf455a5d147a85ff9
Zachary Berkenkotter <109328944+zachberk@users.noreply.github.com>
2024-06-20 14:03:59 +0800

    Do exercise for Module 2

--- youtube/react/pedrotech/mod1and2/exercise/src/App.js
+++ youtube/react/pedrotech/mod1and2/exercise/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Job salary={90000} position="Senior SDE" company="Amazon" />
+      <Job salary={12000} position="Junior SDE" company="Google" />
+      <Job salary={10000} position="Project Manager" company="Netflix" />
+    </div>
+  );
+}
+
+const Job = (props) => {
+  return (
+    <div>
+      <h2>Salary: {props.salary}</h2>
+      <h2>Position: {props.position}</h2>
+      <h2>Company: {props.company}</h2>
     </div>
   );
 }
