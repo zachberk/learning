dfa896e79cf0108ba30aaa7c66304da719a1f34a
Zachary Berkenkotter <109328944+zachberk@users.noreply.github.com>
2024-06-20 13:41:08 +0800

    Follow along module 2

--- youtube/react/pedrotech/mod1-react-basics/src/App.js
+++ youtube/react/pedrotech/mod1-react-basics/src/App.js
@@ -1,9 +1,54 @@
 import './App.css';
 
 function App() {
+  const name1 = GetNameComponent(); // assigned using return value of the component GetNameComponent
+  const age1 = <h2>Age: 21</h2>;
+  const email1 = <h2>Email: hello@world.com</h2>;
+  const user1 = (
+    <div>
+      <h1>User1: JS Variable</h1>
+      {name1}
+      {age1}
+      {email1}
+    </div>
+  );
+
   return (
-    <div className="App">
-      
+  <div className="App">
+    {user1}
+    <br></br>
+    <User2 name="Pedro" age={21} email="pedro@world.com" />
+    <br></br>
+    <User2 name="Jake" age={18} email="jake@world.com" />
+    <br></br>
+    <User2 name="Zach" age={22} email="zach@world.com" />
+  </div>
+  );
+}
+
+const props_example = {
+  name: "Pedro",
+  age: 21,
+  email: "pedro@world.com"
+};
+
+// JS Function:
+// const getName = () => {
+//   return "Pedro";
+// }
+
+// React Components:
+const GetNameComponent = () => {
+  return <h1>Name: Pedro</h1>;
+};
+
+const User2 = (props) => {
+  return (
+    <div>
+      <h1>User2: React Component</h1>
+      <h2>Name: {props.name}</h2>
+      <h2>Age: {props.age}</h2>
+      <h2>Email: {props.email}</h2>
     </div>
   );
 }
