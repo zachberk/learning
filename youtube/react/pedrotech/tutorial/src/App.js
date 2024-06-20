72197d20e323e5b0613c82f5e3fa7d23b425a448
Zachary Berkenkotter <109328944+zachberk@users.noreply.github.com>
2024-06-20 15:38:05 +0800

    Follow along module 3

--- youtube/react/pedrotech/tutorial/src/App.js
+++ youtube/react/pedrotech/tutorial/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { User } from './User.js';
 
 function App() {
-  return (
-  <div className="App">
+  const users = [
+    { name: "Hanee", age: 48 }, 
+    { name: "Pau", age: 30 }, 
+    { name: "Bong", age: 50 },
+    { name: "Mamita",age: 70}
+  ];
 
-  </div>
+  return (
+    <div className="App">
+      <h1> {users.map((user, key) => {
+              return <User name={user.name} age={user.age} />
+          })}</h1>
+    </div>
   );
 }
 
