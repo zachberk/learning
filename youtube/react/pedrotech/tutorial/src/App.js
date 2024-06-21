4d015b4ca986f149627a9ad7d8b05bd97a0d7cd0
Zachary Berkenkotter <109328944+zachberk@users.noreply.github.com>
2024-06-21 12:05:30 +0800

    Wipe App.js for next module

--- youtube/react/pedrotech/tutorial/src/App.js
+++ youtube/react/pedrotech/tutorial/src/App.js
@@ -3,26 +3,10 @@ import { useState } from "react";
 
 // Increase, decrease, and set to zero button on a live counter
 function App() {
-  const [count, setCount] = useState(0);
-
-  const increaseCount = () => {
-    setCount(count + 1);
-  };
-
-  const decreaseCount = () => {
-    setCount(count - 1);
-  };
-
-  const zeroCount = () => {
-    setCount(0);
-  };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }} className="App">
-      <button onClick={increaseCount}> Increase </button>
-      <button onClick={decreaseCount}> Decrease </button>
-      <button onClick={zeroCount}> Set to Zero </button>
-      {count}
+    <div className="App">
+      
     </div>
   );
 }
