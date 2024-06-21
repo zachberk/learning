d66c59600dc28bf882496399cc70533adef31d2e
Zachary Berkenkotter <109328944+zachberk@users.noreply.github.com>
2024-06-21 12:02:12 +0800

    Finish module 4 on useState hook

--- youtube/react/pedrotech/tutorial/src/App.js
+++ youtube/react/pedrotech/tutorial/src/App.js
@@ -1,19 +1,28 @@
 import "./App.css";
-import { Planet } from "./Planet.js"; // React component of Planet
-import { planets } from "./planets.js"; // list of planets
+import { useState } from "react";
 
-// Exercise: display planet's name only if they ARE a gas planet
+// Increase, decrease, and set to zero button on a live counter
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  const decreaseCount = () => {
+    setCount(count - 1);
+  };
+
+  const zeroCount = () => {
+    setCount(0);
+  };
+
   return (
-    <div className="App">
-      {planets.map(
-        (planet, key) =>
-          planet.isGasPlanet && (
-            <h2>
-              <Planet name={planet.name} key={key} />
-            </h2>
-          )
-      )}
+    <div style={{ display: 'flex', flexDirection: 'column' }} className="App">
+      <button onClick={increaseCount}> Increase </button>
+      <button onClick={decreaseCount}> Decrease </button>
+      <button onClick={zeroCount}> Set to Zero </button>
+      {count}
     </div>
   );
 }
