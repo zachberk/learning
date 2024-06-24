711672a399fe14f11302ce6add2a029647e896eb
Zachary Berkenkotter <109328944+zachberk@users.noreply.github.com>
2024-06-24 14:13:01 +0800

    Create Text component for Module 6 - useEffect

--- youtube/react/pedrotech/tutorial/src/App.js
+++ youtube/react/pedrotech/tutorial/src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import { useState } from "react";
 
-// Increase, decrease, and set to zero button on a live counter
 function App() {
 
   return (
     <div className="App">
-      
+
     </div>
   );
 }
