6db6954cbab99d146182c398a57355c846bfa5c8
Zachary Berkenkotter <109328944+zachberk@users.noreply.github.com>
2024-07-20 21:35:21 +0800

    Add react-router-dom components

--- youtube/react/pedrotech/tutorial/src/App.js
+++ youtube/react/pedrotech/tutorial/src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Menu } from "./pages/Menu";
+import { Contact } from "./pages/Contact";
+import { Navbar } from "./Navbar";
 
 function App() {
-
   return (
     <div className="App">
-
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
